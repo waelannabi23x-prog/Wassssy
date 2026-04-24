73f693396c12a1855a9d8a9bc1ae5c614ffd916f
flixoflixo23-alt <flixoflixo23@gmail.com>
2026-04-24 09:16:14 +0100

    test: remove auth check from /all

--- handlers/group_commands.js
+++ handlers/group_commands.js
@@ -4,7 +4,6 @@ const { showAllMembers, tagAll, muteAll, unmuteAll } = require('./group_admin');
 function setupGroupCommands(bot) {
   bot.command('all', async ctx => {
     if (!['supergroup','group'].includes(ctx.chat.type)) return;
-    if (!ctx.isOwner && !ctx.isAdmin) return ctx.reply('🚫 للمشرفين فقط').catch(() => {});
     try { await showAllMembers(ctx, ctx.chat.id); }
     catch(e) { ctx.reply('❌').catch(() => {}); }
   });
