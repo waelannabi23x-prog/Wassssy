a7ffcfcab9cfe95b76ae9b83ee773aad7517b9d5
chouchanewael08-byte <Chouchanewael08@gmail.com>
2026-05-31 20:56:31 +0100

    fix: pm2 to deps, remove inline_query, fix cluster instances, clean sh files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'study-bot',
     script: 'index.js',
-    instances: 1,
+    instances: process.env.WEB_CONCURRENCY || 2,
     exec_mode: 'cluster',
     max_memory_restart: '380M',
     node_args: '--max-old-space-size=380 --optimize-for-size --expose-gc',
