ad6f813e632dcb4ec4f047414d5fb6e223f1b462
chouchanewael08-byte <Chouchanewael08@gmail.com>
2026-06-08 08:17:44 +0100

    fix: allow games_ callbacks in groups

--- bot/callbacks.js
+++ bot/callbacks.js
@@ -639,7 +639,8 @@ module.exports.registerCallbacks = function(bot, deps) {
           || data.startsWith('mlr_') || data.startsWith('mar_')
           || data.startsWith('grp_mute_1h_') || data.startsWith('grp_ban_')
           || data.startsWith('grp_warns_') || data.startsWith('grp_perms_')
-          || data.startsWith('grp_restrict_') || data.startsWith('grp_unrestrict_');
+          || data.startsWith('grp_restrict_') || data.startsWith('grp_unrestrict_')
+          || data.startsWith('games_');
         if (!_grpOk)
           return ctx.answerCbQuery('👉 استخدم البوت في الخاص', { show_alert: true }).catch(err => { require('../utils/logger').debug("[silent]", err.message); });
       }
