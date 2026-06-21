6e9d44636e6c364322a379a24c7b1f5e1f42d5e2
chouchanewael08-byte <Chouchanewael08@gmail.com>
2026-06-21 11:40:50 +0100

    chore: إضافة ESLint

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    languageOptions: { ecmaVersion: 2022, sourceType: "commonjs" },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "warn",
+      "no-unreachable": "error"
+    }
+  }
+];
